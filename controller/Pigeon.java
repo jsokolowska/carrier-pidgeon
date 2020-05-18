9d77aa1f90c3e04be48f132792514a60e944ca06
jsokolowska <joana.sokolowska@gmail.com>
2020-05-18 20:48:53 +0200

    Improved aligment on login and sign up view, added sample messages to conversation view and added sample color scheme

--- controller/Pigeon.java
+++ controller/Pigeon.java
@@ -3,8 +3,6 @@ package controller;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.jetbrains.annotations.NotNull;
 
@@ -15,7 +13,7 @@ public class Pigeon extends Application {
     @Override
     public void start(@NotNull Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("/resources/connections.fxml"));
+        loader.setLocation(getClass().getResource("/resources/contacts.fxml"));
         Scene scene = new Scene(loader.load());
         primaryStage.setScene(scene);
         primaryStage.show();
