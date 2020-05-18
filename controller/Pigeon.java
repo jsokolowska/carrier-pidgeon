ac1b30546f61f4746a7d8fdd1dece4aebddd18ba
jsokolowska <joana.sokolowska@gmail.com>
2020-05-18 18:02:26 +0200

    Added login and sign up screens, added ids to fxml elements, added sample start method for javafx application

---
+++ controller/Pigeon.java
@@ -0,0 +1,23 @@
+package controller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import org.jetbrains.annotations.NotNull;
+
+public class Pigeon extends Application {
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+    @Override
+    public void start(@NotNull Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/resources/connections.fxml"));
+        Scene scene = new Scene(loader.load());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
